4e4283906ce7a0f86492c44010c0e02fa8db49e7
favasee <156898469+favasee@users.noreply.github.com>
2024-01-25 12:20:39 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,36 @@
+function runAnimations() {
+  setTimeout(() => {
+    document.querySelector("#id-6823").classList.add("animationClass-6823");
+    document.querySelector("#id-6826").classList.add("animationClass-6826");
+    document.querySelector("#id-6827").classList.add("animationClass-6827");
+    document.querySelector("#id-6828").classList.add("animationClass-6828");
+    document.querySelector("#id-9332").classList.add("animationClass-9332");
+    document
+      .querySelector(".thank-you-for-y-1-475-0")
+      .classList.add("animationClass-6829-6829-0");
+    document.querySelector("#id-6829").classList.add("animationClass-6829");
+    document.querySelector("#id-916").classList.add("animationClass-916");
+    document
+      .querySelector(".thank-you-1-3740-0")
+      .classList.add("animationClass-6830-6830-0");
+    document.querySelector("#id-6830").classList.add("animationClass-6830");
+    document
+      .querySelector(".follow-us-1-204-0")
+      .classList.add("animationClass-9333-9333-0");
+    document.querySelector("#id-9333").classList.add("animationClass-9333");
+    document.querySelector("#id-1105").classList.add("animationClass-1105");
+    document
+      .querySelector("#id-97394")
+      .classList.add("animationClass-null97394");
+    document.querySelector("#id-6833").classList.add("animationClass-6833");
+    document.querySelector("#id-6834").classList.add("animationClass-6834");
+    document
+      .querySelector("#id-6834 > div:first-child")
+      .classList.add("bgClass-97397");
+    document.querySelector("#id-6835").classList.add("animationClass-6835");
+    document
+      .querySelector("#id-6835 > div:first-child")
+      .classList.add("bgClass-97398");
+  }, 800);
+}
+runAnimations();
